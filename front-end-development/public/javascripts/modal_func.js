c19ece1799114a9d528f361a2f8833c46ab282d5
Karthic Rao <kartronics85@gmail.com>
2014-02-22 12:13:11 +0530

    Registration form validated

--- front-end-development/public/javascripts/modal_func.js
+++ front-end-development/public/javascripts/modal_func.js
@@ -1,11 +1,32 @@
 $('document').ready(function(){
-	    //$.validate();
 		$('#mymodal').modal('show');
 		
 		$('#save').click(function(){
 			console.log("inside save");
 			var roomie = $('#roomiename').val();
 			console.log(roomie);
+		});
+		$('#myform').validate({
+			rules:{
+				roomie:{
+					required:true,
+					minlength:3
+				},
+				mail:{
+					required:true,
+					email:true
+				}
+			},
+			messages:{
+				roomie:{
+					required:"Identify yourself, dude!",
+					minlength:"Too short a name to be a roomie!"
+				},
+				mail:{
+					required:"Can't get away without entering eeeMailId!",
+					email:"LOL.I know this aint a valid address format!:D"
+				}
+			}
 		});
 
 	
