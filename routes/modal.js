a1894e4c49d659f9a830b518ea7b21320ff13a7e
Karthic Rao <kartronics85@gmail.com>
2014-02-24 11:31:25 +0530

    Login working

--- routes/modal.js
+++ routes/modal.js
@@ -10,7 +10,22 @@ exports.getModalData = function(req,res){
 	console.log("control inside Post data ");
 	console.log(req.body.roomie);
 	console.log(req.body.mail );
+	core.isUser("users",req.body.roomie,function(err,result){
+		if(result){
+			core.getUserMail("userInfo",req.body.roomie,function(err,reply){
+				if(req.body.mail===reply){
+					req.session.name = req.body.roomie;
+					res.redirect('/');
+				}else{
+					console.log("Mail Id incorrect");
+					res.render('modal');
 
-	req.session.name = req.body.roomie;
-	res.redirect('/');
+				}
+			});
+		}else{
+			console.log("User Name is incorrect");
+			res.render('modal');
+		}
+	});
+	
 }
