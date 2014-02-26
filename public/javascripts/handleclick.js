1ca4a1d53f816f5b6944b54e2bc1fbb6029a9b5c
Karthic Rao <kartronics85@gmail.com>
2014-02-26 05:47:02 +0530

    Template with button for settling expenses and go to home added

---
+++ public/javascripts/handleclick.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  		$('#home').click(function(){
+  		var url="http://"+location.host+'/';
+  		console.log('Home button clicked');
+  		window.location=url;
+  	});
+  		$('#settle').click(function(){
+  		var accept=confirm("Are you Sure you want to Settle all Expenes??");
+  		if(acept){
+			var url="http://"+location.host+'/settlement/clear';
+			console.log('Settle clicked');
+			window.location=url;
+			}
+		});
+	});   
