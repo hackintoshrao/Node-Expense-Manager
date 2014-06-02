d04f88a8fd96b5882ba35831bc2ca9add3de5be0
passion <passion@passion-Inspiron-N4030.(none)>
2014-06-02 21:21:55 +0530

    Zombie tets are running successfully

--- zombie-test/test_signup.js
+++ zombie-test/test_signup.js
@@ -10,17 +10,53 @@ var assert = require('assert'),
 		app.server.close(done);
 	});*/
 
-	describe('Signup Test',function(){
-		describe('sign up form test',function(){
-			it('This should load the sign up form',function(done){
+	describe('Login tests',function(){
+		describe('Using username and password to login and test',function(){
+			it('Checking for the right page',function(done){
 				
 				virtualBrowser.visit('http://127.0.0.1:3000/modal',function(err,browser){
 					if(err)
 						throw err;
-					console.log('Zombie visited the page,page loaded ');	
+					console.log('Z.Zombie visited the page,page loaded	 ');	
 					assert.ok(browser.success,'page loaded');
-
+					assert.equal(browser.text('h4'),'Welcome Roomie');
+					var form = browser.query('form');
+					assert(form,'form present on the page');
+					assert.equal(form.method,'post','POSTing present on the page');
+					assert.equal(form.action,'/modal','Post to modal exists');
 					done();
+				});
+			});
+			it('Testing for the reply for wrong username ',function(done){
+				virtualBrowser.visit('http://127.0.0.1:3000/modal',function(err,browser){
+					browser
+						.fill('#roomiename','ab')
+						.fill('#email','kartronics85@yahoo.com')
+						assert.equal(browser.text('.error'),'Too short a name to be a roomie!')
+						done();
+				});
+			});
+			it('Testing for the reply for invalid email format ',function(done){
+				virtualBrowser.visit('http://127.0.0.1:3000/modal',function(err,browser){
+					browser
+						.fill('#roomiename','abcde')
+						.fill('#email','kartronics85yahoo.com')
+						assert.equal(browser.text('.error'),'LOL.I know this aint a valid address format!:D');
+						done();
+				});
+			});
+			it('Logging in and testing it ',function(done){
+				virtualBrowser.visit('http://127.0.0.1:3000/modal',function(err,browser){
+					browser
+						.fill('#roomiename','rao')
+						.fill('#email','kartronics85@yahoo.com')
+						.pressButton('#save',function(err){
+							if(err)
+								throw err;
+							console.log('Z.Login test successful');
+							assert.equal(browser.text('h3'),'An Expense Manager application built with Socket.IO,Twitter Bootstrap, Node.js, Express,Redis and tested using mocha');
+							done();
+						});
 				});
 			});
 
